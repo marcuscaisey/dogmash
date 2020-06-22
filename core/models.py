acea3f0ddf5b22b786b21843ae94517fe0c36758
Marcus Caisey <marcus@teckna.com>
2020-06-22 17:46:06 +0100

    Configure image field on Dog

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db.models import Count, Q
 
 class Dog(models.Model):
     rating = models.DecimalField(decimal_places=3, max_digits=7)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="uploads/%Y/%m/%d/", width_field="width", height_field="height")
 
     def __str__(self):
         return f"pk={self.pk}, rating={self.rating}"
