6e7809d024cb01835116532572af80da8a3aee46
Marcus Caisey <marcus@teckna.com>
2020-06-22 15:55:44 +0100

    Add rank property to Dog

--- core/models.py
+++ core/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.db.models import Count, Q
 
 
 class Dog(models.Model):
     rating = models.DecimalField(decimal_places=3, max_digits=7)
     image = models.ImageField()
+
+    def __str__(self):
+        return f"pk={self.pk}, rating={self.rating}"
+
+    @property
+    def rank(self):
+        # Your rank is the number of distinct ratings greater than your rating + 1.
+        return Dog.objects.aggregate(rank=Count("rating", filter=Q(rating__gt=self.rating), distinct=True) + 1)["rank"]
