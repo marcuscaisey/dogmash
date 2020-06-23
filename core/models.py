f91995e9e743bb7e0909ba2d1692bfe255f114ad
Marcus Caisey <marcus@teckna.com>
2020-06-23 01:50:19 +0100

    Resize images on upload

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models import Count, Q
 
+from PIL import Image
+
 
 class Dog(models.Model):
     rating = models.DecimalField(decimal_places=3, max_digits=7, default=1000)
@@ -13,3 +15,19 @@ class Dog(models.Model):
     def rank(self):
         # Your rank is the number of distinct ratings greater than your rating + 1.
         return Dog.objects.aggregate(rank=Count("rating", filter=Q(rating__gt=self.rating), distinct=True) + 1)["rank"]
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image.width > 300 or self.image.height > 400:
+            image = Image.open(self.image.path)
+
+            if image.width > 300:
+                aspect_ratio = self.image.width / self.image.height
+                image = image.resize((300, round(300 / aspect_ratio)))
+
+            if image.height > 400:
+                image = image.crop((0, image.height - 400, 300, image.height))
+
+            image.save(self.image.path)
+
